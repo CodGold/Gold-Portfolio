d15a6c2876ba3e94c69a5a7db954f9aa7a8ffd19
Gold <4628cc@naver.com>
2022-09-21 21:05:32 +0900

    love

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function revealMyInfo(myName, className, color) {
+function revealMyInfo(myName, className, color, animationName) {
   let htmlElement = document.querySelector(className);
 
   htmlElement.innerHTML = myName;
   htmlElement.style.color = color;
+
+  htmlElement.style.animationDuration = "1s";
+
+  if (animationName === "bgFadeOut") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "slideIn") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "heartBeat") {
+    htmlElement.style.animationName = animationName;
+  }
 }
